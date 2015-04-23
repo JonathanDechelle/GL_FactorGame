8c3e6b0005920686349af23235d11638181f857d
JonathanDechelle <jonathan.dechelle@hotmail.com>
2015-04-22 22:23:43 -0500

    StaticFile

---
+++ GL_FactorGame/GL_FactorGame/StaticHandle.cpp
@@ -0,0 +1,6 @@
+#include "StaticHandle.h"
+
+GLuint StaticHandle::rendering_program = 0;
+GLuint StaticHandle::mv_location = 0;
+GLuint StaticHandle::proj_location = 0;
+GLuint StaticHandle::lookAtMatrix_Location = 0;
---
+++ GL_FactorGame/GL_FactorGame/StaticHandle.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "..\Include\glew.h"
+#include "..\Include\glut.h"
+
+class StaticHandle
+{
+public:
+	static GLuint rendering_program;
+	static GLuint mv_location;
+	static GLuint proj_location;
+	static GLuint lookAtMatrix_Location;
+};
+
