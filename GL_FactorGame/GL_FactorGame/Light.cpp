893542b221993d58c8e1938de89cc7446514492c
JonathanDechelle <jonathan.dechelle@hotmail.com>
2015-04-28 07:25:08 -0500

    Light Files

---
+++ GL_FactorGame/GL_FactorGame/Light.cpp
@@ -0,0 +1,28 @@
+#include "Light.h"
+
+Light::Light(void)
+{
+	//Light Direction
+	Light_Direction = vec4(0.0, 0.0, 0.0, 0.0);
+	Light_Brightness = 1;
+	Light_Radius = 0.75f;
+}
+
+
+Light::~Light(void)
+{
+}
+
+void Light::Initialize(int rendering_program)
+{
+	lightDir_location = glGetUniformLocation(rendering_program,"Light_Direction");
+	lightBrightness_location = glGetUniformLocation(rendering_program,"Light_Brightness");
+	lightRadius_location = glGetUniformLocation(rendering_program, "Light_Radius");
+}
+
+void Light::Update()
+{
+	glUniform4f(lightDir_location,Light_Direction[0],Light_Direction[1],Light_Direction[2],Light_Direction[3]);
+	glUniform1f(lightBrightness_location, Light_Brightness);
+	glUniform1f(lightRadius_location, Light_Radius);
+}
